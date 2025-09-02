087100b8ff28ffb52e4da7377944351d8d5ea091
Harikrishnan.U <lawliet.oradoraudaz@gmail.com>
2025-09-02 16:18:41 -0700

    Create fizzorbuzz.py

---
+++ fizzorbuzz.py
@@ -0,0 +1,12 @@
+#fizzorbuzz a simple code that prints fizz if input is divisible by 3 and buzz if 5, fizzbuzz if both and bumber in none
+
+x=int(input("Enter your input"))
+if (x%3 == 0):
+    print("Fizz")
+elif (x%5==0):
+    print("Buzz")
+elif(x%5==0 or x%3==0):
+    print("Buzzfizz")
+else:
+    print("Bummer")
+
